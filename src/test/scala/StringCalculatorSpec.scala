6e425fc2ceeb73179052ac5d383642829db11d7d
Javier Lopez <javisan81@hotmail.com>
2020-01-10 15:24:09 +0100

    calculate the sum of one number string

--- src/test/scala/StringCalculatorSpec.scala
+++ src/test/scala/StringCalculatorSpec.scala
@@ -2,9 +2,14 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class StringCalculatorSpec extends FlatSpec with Matchers {
 
-  def calculate(value: String): Int = 0
+  def calculate(value: String): Int = if (value == "") 0 else value.toInt
 
   it should "be 0 for empty string" in {
     calculate("") should equal(0)
+  }
+
+  it should "be same for unique number" in {
+    calculate("1") should equal(1)
+    calculate("2") should equal(2)
   }
 }
